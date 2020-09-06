fe0df4b2a049d6ec4c1a4e467ef63792d85b54eb
Stephen Gary <sgary0@protonmail.com>
2020-09-06 19:21:13 -0400

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const helmet = require('helmet');
-const server = express();
+const cors = require('cors');
+const socketio = require('socket.io');
+const http = require('http');
+const router = require('./router.js');
 
-server.use(helmet());
-server.use(express.json());
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users.js');
 
-server.get('/', (req, res) => {
-    res.send('Chat App Server');
+const PORT = process.env.PORT || 8008;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server).listen(server);
+
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
+
+    socket.join(user.room);
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User has left!!')
+  });
 });
 
-const port = process.env.PORT || 8008;
-server.listen(port);
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(router);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+
+app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
